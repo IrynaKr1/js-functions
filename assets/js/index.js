6f688482894d8bfd869d01a61f79070067ba45e8
IrynaKr1 <krutsiryna@gmail.com>
2025-06-03 16:43:30 +0200

    Add functions that calculate triangle and cone area, task 4

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,7 @@ function checkMultiplicity(num1, num2) {
   return num1 % num2 === 0 ? true : false;
 }
 
-//3
+// 3
 const isTriangle = function (a, b, c) {
   if (a > 0 && b > 0 && c > 0) {
     if (a + b > c && a + c > b && b + c > a) {
@@ -21,3 +21,24 @@ const isTriangle = function (a, b, c) {
   }
 };
 
+// 4
+
+const triangleArea = function (base, height) {
+  if (base > 0 && height > 0) {
+    return 0.5 * base * height;
+  } else {
+    return 'Error: the sides must be positive numbers.';
+  }
+};
+
+console.log(triangleArea(2,5));
+
+function coneSurfaceArea (radius, slantHeight) {
+     if (radius > 0 && slantHeight > 0) {
+    return Math.PI * radius *slantHeight;
+  } else {
+    return 'Error: the sides must be positive numbers.';
+  }
+};
+
+console.log (coneSurfaceArea(2,3));
