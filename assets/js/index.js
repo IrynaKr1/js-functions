6fa85c15d1c8d7aaca40914ff2fbe89b0a47b8b2
IrynaKr1 <krutsiryna@gmail.com>
2025-06-03 11:54:08 +0200

    Add function checkMultiplicity, task 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,8 @@ function isWorkingAgePerson(age) {
   return age >= 16 && age <= 64 ? true : false;
 }
 
-console.log(isWorkingAgePerson(88));
+// 2
+function checkMultiplicity(num1, num2) {
+  return num1 % num2 === 0 ? true : false;
+}
+
