a3d9e6e68bc04dea5ff112e6a8a7251cd1256ecd
IrynaKr1 <krutsiryna@gmail.com>
2025-06-03 12:25:44 +0200

    Add function expression isTriangle, task 3

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,16 @@ function checkMultiplicity(num1, num2) {
   return num1 % num2 === 0 ? true : false;
 }
 
+//3
+const isTriangle = function (a, b, c) {
+  if (a > 0 && b > 0 && c > 0) {
+    if (a + b > c && a + c > b && b + c > a) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+};
+
